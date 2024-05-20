49cdbbef813185176cb9094b4473e5893150542f
Raghu <raghupatil19@gmail.com>
2024-05-20 21:44:05 +0530

    enabled CORS for localhost:4200

--- src/main/java/raghudev/transactional/TransactionalApplication.java
+++ src/main/java/raghudev/transactional/TransactionalApplication.java
@@ -2,9 +2,24 @@ package raghudev.transactional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TransactionalApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TransactionalApplication.class, args);
